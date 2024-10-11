4af28cdfec3d3c57a45947e1228101e38de7a335
myketef <myketef@gmail.com>
2024-10-11 22:38:54 +0300

    Carousel Completed

---
+++ react-state-carousel-coins/src/photos.js
@@ -0,0 +1,20 @@
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+
+const photos = [
+  {
+    src: image1,
+    caption: "Photo by Samsung Memory on Unsplash"
+  },
+  {
+    src: image2,
+      caption: "Photo by Tabea Schimpf on Unsplash"
+  },
+  {
+    src: image3,
+      caption: "Photo by Andrej Lišakov on Unsplash"
+  }
+]
+
+export default photos
